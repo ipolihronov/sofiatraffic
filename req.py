04af4be102e807082c47b0b3b125cca24e7d100d
ivo <ivailomet@gmail.com>
2018-06-23 15:07:18 +0300

    req.py working

--- req.py
+++ req.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import requests
 import json
-from sys import argv
+import urllib.request
 stops = []
 vehicle = []
 
@@ -10,15 +11,21 @@ class Stop():
         self.name = name
         self.id = id
 
-class Vehicle():
+class Vechile():
     def __init__(self, type, number):
         self.type = type
         self.number = number
+    time = []
 
 def extractAllStops():
-    rAllStop =requests.get('https://www.sofiatraffic.bg/interactivecard/stops/geo?bbox=25,51,27,53,EPSG:3857')
-    allData = rAllStop.json()
-    for stop in allData['features']:
+    # rAllStop = requests.get('https://www.sofiatraffic.bg/interactivecard/stops/geo?bbox=25,51,27,53,EPSG:3857,EPSG:3857')
+    # allData = rAllStop.json()
+    # with open('stopsCache.json', 'w') as outfile:
+    #     json.dump(allData, outfile)
+
+    with open('stopsCache.json') as f:
+        data = json.load(f)
+    for stop in data['features']:
         stops.append(Stop(stop['properties']['code'], stop['properties']['name'], stop['id']))
 
 def schedule(code):
@@ -30,15 +37,17 @@ def schedule(code):
     rStop = requests.get('https://www.sofiatraffic.bg/interactivecard/virtual_panel?', params=step)
     dataForStop = rStop.json()
 
+    lineIndex = 0
     for line in dataForStop['virtual_panel_data']['lines']:
-        vehicle.append(Vehicle(line['transport'], line['name']))
+        vehicle.append(Vechile(line['transport'], line['name']))
+        print(vehicle[lineIndex].type, vehicle[lineIndex].number)
+        lineIndex += 1
         for z in line['cars']:
             departureTime = z['departure_time']
-            vehicle[-1].time = departureTime
+            print(departureTime)
 
 extractAllStops()
-schedule('2731')
+schedule(879)
 
-
-for v in vehicle:
-    print(v.type, v.number, v.time)
+# for v in vehicle:
+#     print(v.type, v.number, v.time)
