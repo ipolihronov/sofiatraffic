27d591f6b23f52e5c501a7d66c58a13f36bac518
ivo <ivailomet@gmail.com>
2018-06-23 16:48:50 +0300

    flask app updated. Now working

--- app.py
+++ app.py
@@ -2,48 +2,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
-import requests
+from flask import request
+from req import schedule, extractAllStops
+from req import vehicle
+
 # create app
 app = Flask(__name__)
 
 import requests
-import json
-from sys import argv
-stops = []
-vehicle = []
-
-class Stop():
-    def __init__(self, code, name, id):
-        self.code = code
-        self.name = name
-        self.id = id
-
-class Vehicle():
-    def __init__(self, type, number):
-        self.type = type
-        self.number = number
-
-def extractAllStops():
-    rAllStop =requests.get('https://www.sofiatraffic.bg/interactivecard/stops/geo?bbox=25,51,27,53,EPSG:3857')
-    allData = rAllStop.json()
-    for stop in allData['features']:
-        stops.append(Stop(stop['properties']['code'], stop['properties']['name'], stop['id']))
-
-def schedule(code):
-    for s in stops:
-        if code == s.code:
-            x = s.id
-
-    step = {'stop_id' : x}
-    rStop = requests.get('https://www.sofiatraffic.bg/interactivecard/virtual_panel?', params=step)
-    dataForStop = rStop.json()
-
-    for line in dataForStop['virtual_panel_data']['lines']:
-        vehicle.append(Vehicle(line['transport'], line['name']))
-        for z in line['cars']:
-            departureTime = z['departure_time']
-            vehicle[-1].time = departureTime
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -52,28 +18,24 @@ def index():
         # show html form
         return '''
             <form method="post">
+                <p> Въведи номер на спирката </p>
                 <input type="text" name="expression" />
-                <input type="submit" value="Calculate" />
+                <input type="submit" value="Готово" />
             </form>
         '''
     elif request.method == 'POST':
         expression = request.form.get('expression')
-        result = eval(expression)
-        result = square(result)
+        try:
+            result = eval(expression)
+        except:
+            return render_template('problem.html')
         extractAllStops()
-        schedule(2132)
-        return 'result: %s' % result
+        try:
+            schedule(result)
+        except:
+            return render_template('problem.html')
+        return render_template('index.html', vehicle = vehicle)
 
-@app.route('/result')
-def result():
-    time = vehicle[-1].time
-    type = vehicle[-1].type
-    number = vehicle[-1].number
-    return render_template('index.html', time = time, type = type, number = number)
-
-def square(a):
-    a = a*a
-    return a
 
 # run app
 if __name__ == '__main__':
